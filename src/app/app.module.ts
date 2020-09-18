c2f3a4ae440066011c0239cbb2ff892487d51bc6
empelogi <belogiannieleni@gmail.com>
2020-09-18 16:21:40 +0300

    [ADD] Exercise implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BasicFormModule } from './basic-form/basic-form.module';
+import {BasicFormComponent} from './basic-form/basic-form/basic-form.component';
 
 @NgModule({
   declarations: [
--- src/app/basic-form/basic-form/basic-form.component.ts
+++ src/app/basic-form/basic-form/basic-form.component.ts
@@ -14,10 +14,35 @@ export class BasicFormComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      firstName: new FormControl(null, Validators.required),
-      lastName: new FormControl(null, Validators.required),
+      firstName: new FormControl(null,[
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+    
+      lastName: new FormControl(null,Validators.required),
+      experience : new FormControl(null,Validators.required),
+      angPreference : new FormControl(null,Validators.required),
+      favLan : new FormControl(null,Validators.required),
+      version : new FormControl(null,Validators.required)
+
+    });
+
+    this.form.get('favouriteLanguage').valueChanges.subscribe(value => {
+
+      const jsVersionFormControl = this.form.get('jsversion');
+
+      if (value === 'JS') {
+        jsVersionFormControl.setValidators(Validators.required);
+      } else {
+        jsVersionFormControl.clearValidators();
+      }
+      jsVersionFormControl.updateValueAndValidity();
     });
 
   }
+
+// onSubmit(){
+//   console.log(this.form.value);
+// }
 
 }
